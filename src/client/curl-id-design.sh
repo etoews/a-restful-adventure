7b5881bd6467a7685975b52ae13fdfb34fa3b64b
Everett Toews <everett.toews@rackspace.com>
2015-02-14 12:13:43 -0600

    First cut at client flow

--- src/client/curl-id-design.sh
+++ src/client/curl-id-design.sh
@@ -1,8 +1,39 @@
-HOST=http://api.dungeoncrawler.io
+#!/bin/bash
 
-# Create a character
-curl -X POST -H 'Content-type: application/json' -d '{ "name": "$NAME" }'
+# requirements: jq (see http://stedolan.github.io/jq/)
 
-# Pick a character (just grab the first one for simplicity sake)
-CHARACTER_ID=$(curl -X GET -H 'Accept: application/json' $HOST/characters | jq -r .characters[0].id)
+set -euo pipefail
+
+HOST=http://localhost:5000
+
+echo "Create a character"
+curl -s -X POST -H 'Content-type: application/json' -d '{ "name": "Steve" }' $HOST/characters
+
+printf "\n\n"
+
+echo "Get characters"
+CHARACTERS=$(curl -s -X GET -H 'Accept: application/json' $HOST/characters)
+echo $CHARACTERS
+CHARACTER_ID=$(echo $CHARACTERS | jq -r .characters[0].id)
+echo "Picked character id: $CHARACTER_ID"
+
+printf "\n\n"
+
+echo "Get a character"
+curl -s -X GET -H 'Accept: application/json' $HOST/characters/$CHARACTER_ID
+
+printf "\n\n"
+
+echo "Get dungeons"
+DUNGEONS=$(curl -s -X GET -H 'Accept: application/json' $HOST/dungeons)
+echo $DUNGEONS
+DUNGEON_ID=$(echo $DUNGEONS | jq -r .dungeons[0].id)
+ENTRANCE_ROOM_ID=$(echo $DUNGEONS | jq -r .dungeons[0].entrance_room_id)
+echo "Picked dungeon id: $DUNGEON_ID"
+echo "Entrance room id: $ENTRANCE_ROOM_ID"
+
+printf "\n\n"
+
+echo "Get a dungeon"
+curl -s -X GET -H 'Accept: application/json' $HOST/dungeons/$DUNGEON_ID
 
--- src/client/dcrawler.py
+++ src/client/dcrawler.py
@@ -1,24 +1,79 @@
-from flask import Flask
+from flask import Flask, jsonify
 
-app = Flask(__name__)
+api = Flask(__name__)
 
-@app.route('/')
+
+@api.route('/')
 def index():
     return '<h1>Welcome to the Dungeon</h1>'
 
-@app.route('/characters')
+
+@api.route('/characters', methods=['POST'])
+def create_character():
+    character = {
+        "id": "1234",
+        "name": "Knox Thunderbane"
+    }
+
+    return jsonify(character)
+
+
+@api.route('/characters')
 def get_characters():
     characters = {
         "characters": [
-    {
-      "id": "1234",
-      "name": "Knox Thunderbane",
-      "dungeon_id": "2345",
-      "room_id": "3456"
+            {
+                "id": "1234",
+                "name": "Knox Thunderbane",
+                "dungeon_id": "2345",
+                "room_id": "3456"
+            }
+        ]
     }
-  ]
-}
-  return '<h1>Welcome to the Dungeon</h1>'
+
+    return jsonify(characters)
+
+
+@api.route('/characters/<id>')
+def get_character(id):
+    character = {
+        "id": "1234",
+        "name": "Knox Thunderbane"
+    }
+
+    return jsonify(character)
+
+
+@api.route('/dungeons')
+def get_dungeons():
+    dungeons = {
+        "dungeons": [
+            {
+                "id": "1234",
+                "name": "Dungeon of Doom",
+                "entrance_room_id": "1000"
+            },
+            {
+                "id": "9876",
+                "name": "Dungeon of Hope",
+                "entrance_room_id": "2000"
+            }
+        ]
+    }
+
+    return jsonify(dungeons)
+
+
+@api.route('/dungeons/<id>')
+def get_dungeon(id):
+    dungeon = {
+        "id": "1234",
+        "name": "Dungeon of Doom",
+        "entrance_room_id": "1000"
+    }
+
+    return jsonify(dungeon)
+
 
 if __name__ == '__main__':
-  app.run(debug=True)
+    api.run(debug=True)
